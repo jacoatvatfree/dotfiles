96676c3fe1f382c7d3fb62f1f320312ad93c60f4
jacoatvatfree <jaco@vatfree.com>
2026-03-12 11:35:01 +0200

    Update .config/nvim/lua/custom/plugins/lsp/mason.lua

--- .config/nvim/lua/custom/plugins/lsp/mason.lua
+++ .config/nvim/lua/custom/plugins/lsp/mason.lua
@@ -44,6 +44,9 @@ return {
       ensure_installed = {
         "prettier", -- prettier formatter
         "stylua", -- lua formatter
+         "isort", -- python formatter
+        "black", -- python formatter
+        "pylint",
         "eslint_d",
       },
     })
